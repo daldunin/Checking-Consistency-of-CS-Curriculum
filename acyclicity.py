ce1c9d549ff40376450bc9ee3f940a4c52eb688a
Dmitry Aldunin <d_aldunin@mail.ru>
2019-07-11 17:55:12 +0300

    Added description to code

--- acyclicity.py
+++ acyclicity.py
@@ -1,5 +1,18 @@
 # Uses python3
 
+# Problem: Checking Consistency of CS Curriculum
+# Problem Introduction
+# A Computer Science curriculum specifies the prerequisites for each course as a list of courses that should be
+# taken before taking this course. You would like to perform a consistency check of the curriculum, that is,
+# to check that there are no cyclic dependencies. For this, you construct the following directed graph: vertices
+# correspond to courses, there is a directed edge (𝑢, 𝑣) is the course 𝑢 should be taken before the course 𝑣.
+# Then, it is enough to check whether the resulting graph contains a cycle.
+# Problem Description
+# Task. Check whether a given directed graph with 𝑛 vertices and 𝑚 edges contains a cycle.
+# Input Format. A graph is given in the standard format.
+# Constraints. 1 ≤ 𝑛 ≤ 103, 0 ≤ 𝑚 ≤ 103.
+# Output Format. Output 1 if the graph contains a cycle and 0 otherwise.
+
 import sys
 import numpy
 
@@ -10,7 +23,7 @@ def acyclic_single(adj):
     explore(0)
     # print(post)
     for (v1, v2) in edges:
-        if post[v1 - 1] <= post[v2 - 1]:
+        if post[v1 - 1] <= post[v2 - 1]: # according to theorem
             isacyclic = 0
             break;
     return isacyclic
